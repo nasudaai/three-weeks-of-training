66d78e17d7c2faa747c7a9e6e7fa5ad116d4106a
nasuda <nasuda908@gmail.com>
2026-01-25 05:23:01 +0900

    add front js

---
+++ weeks/first/browser-js/server.mjs
@@ -0,0 +1,20 @@
+import { createServer } from 'node:http';
+import { readFileSync } from 'node:fs';
+//console.log(createServer);
+
+const js = readFileSync("./main.js");
+//console.log(js);
+
+
+const server = createServer((req, res) => {
+  if (req.url === "/main.js") {
+    res.writeHead(200, { "content-type": "application/javascript" });
+    res.end(js);
+    return;
+  }
+  res.end(`<h1>hi</h1> <script src="/main.js"></script>`);
+});
+
+server.listen(3000, () => {
+  console.log("listen http://localhost:3000");
+});
