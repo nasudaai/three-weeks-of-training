2f98ffa6b6de4de1f448b2eb1e69d8e48d44c42f
nasuda <nasuda908@gmail.com>
2026-01-26 06:38:57 +0900

    add about structure

--- weeks/first/structure.mjs
+++ weeks/first/structure.mjs
@@ -1,5 +1,15 @@
-const structure = {
+const args = process.argv.slice(2);
+console.log(args);
 
+if (args.length !== 1 ) {
+  process.exit(1);
+}
+
+const structure = {
+  title: "",
+  body: ""
 };
+
+structure.title = args[0];
 
 console.log(structure);
