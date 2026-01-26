edd487e2e3ac4bae41ff6ef1d01054183fc9a1ef
nasuda <nasuda908@gmail.com>
2026-01-27 02:21:03 +0900

    make func write()

--- weeks/first/argv_and_write.mjs
+++ weeks/first/argv_and_write.mjs
@@ -1,5 +1,8 @@
 const args = process.argv.slice(2);
-console.log(args);
+//console.log(args);
 
-process.stdout.write("test\n");
+function write() {
+  process.stdout.write("test\n");
+}
 process.stdout.write(JSON.stringify(args) + "\n");
+write();
