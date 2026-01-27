ba5d631edba22fb000942e0fcf6c4ac1dcb88e7d
nasuda <nasuda908@gmail.com>
2026-01-28 02:14:13 +0900

    add update

--- weeks/first/argv_and_write.mjs
+++ weeks/first/argv_and_write.mjs
@@ -1,8 +1,13 @@
 const args = process.argv.slice(2);
 //console.log(args);
 
+function jsonStringify() {
+  const json = JSON.stringify({ a: "b" });
+  console.log(json);
+}
 function write() {
   process.stdout.write("test\n");
 }
 process.stdout.write(JSON.stringify(args) + "\n");
 write();
+jsonStringify();
--- weeks/first/std.mjs
+++ weeks/first/std.mjs
@@ -1,5 +1,6 @@
 process.stdin.on("data", (chunk) => {
   console.log(chunk);
+  process.stdout.write(chunk);
 
   if (chunk.toString().trim() === ".exit") {
     process.exit(0);
