39f6524f79542f6f66c5d64a591f0ea9db3ef28a
nasuda <nasuda908@gmail.com>
2026-01-28 09:44:09 +0900

    add arr and usage

--- weeks/first/std.mjs
+++ weeks/first/std.mjs
@@ -1,11 +1,16 @@
 const args = process.argv.slice(2);
 console.log(args);
+console.log("usage: exit -> .exit");
 
 //function write()
 
+const arr = [];
 
 
 process.stdin.on("data", (chunk) => {
+  arr.push(chunk);
+  console.log(arr);
+
   console.log(chunk);
   process.stdout.write(chunk);
 
