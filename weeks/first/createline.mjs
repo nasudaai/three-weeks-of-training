c22a59dc443ade30879fde33efb8d703723ad8ad
nasuda <nasuda908@gmail.com>
2026-01-28 09:57:13 +0900

    add stdin mode prompt

--- weeks/first/createline.mjs
+++ weeks/first/createline.mjs
@@ -1,4 +1,9 @@
 import { readFileSync } from 'node:fs';
+const args = process.argv.slice(2);
+if (args[0] === "c") {
+  process.stdout.write("create lines start! stdin >>> \n");
+  process.exit(0);
+}
 
 const data = readFileSync("sample.txt", "utf-8");
 console.log(data);
