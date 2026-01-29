fa34203acc9de0a1d7d94647440b1d49629e6c58
nasuda <nasuda908@gmail.com>
2026-01-29 09:24:27 +0900

    add condithion

--- virtual-login/main.mjs
+++ virtual-login/main.mjs
@@ -1,10 +1,20 @@
-console.log("log in?");
+let loggedIn = false;
+
+console.log("log in? (yes/no)");
 
 process.stdin.on("data", (chunk) => {
-  if (chunk.toString().trim() === "yes") {
+  const input = chunk.toString().trim();
+  
+  if (input === "yes") {
+    loggedIn = true;
     console.log("log in!");
+//    process.exit(0);
+    console.log("what will you do?")
+  } else if (input === "no") {
+    console.log("cancel");
     process.exit(0);
-  } else {
-    console.error("usage: yes");
+  } else { 
+    console.error("usage: yes | no");
+    process.exit(1);
   }
 });
