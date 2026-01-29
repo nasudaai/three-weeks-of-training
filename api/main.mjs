a283a8fea26d445d7b941b6086834d0d5ec4bc67
nasuda <nasuda908@gmail.com>
2026-01-30 02:07:07 +0900

    add listen

--- api/main.mjs
+++ api/main.mjs
@@ -4,6 +4,11 @@ console.log(createServer);
 const obj = { key: "value" };
 process.stdout.write(JSON.stringify(obj));
 
-const server = createServer((req, res) = {
+const server = createServer((req, res) => {
 //will write
+  res.end(req.url);
+});
+
+server.listen(3000, () => {
+  console.log("listen on http://localhost:3000");
 });
