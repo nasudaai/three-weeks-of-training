a57c5292e5087c214b4fb5b5ad8f52cebe60fa2f
nasuda <nasuda908@gmail.com>
2026-01-31 08:27:39 +0900

    add true -> prompt

--- virtual-login/main.mjs
+++ virtual-login/main.mjs
@@ -4,12 +4,18 @@ console.log("log in? (yes/no)");
 
 process.stdin.on("data", (chunk) => {
   const input = chunk.toString().trim();
+
+  if (loggedIn === true) {
+    console.log(">>>")
+    process.exit(0);
+  }
   
   if (input === "yes") {
     loggedIn = true;
     console.log("log in!");
 //    process.exit(0);
     console.log("what will you do?")
+
   } else if (input === "no") {
     console.log("cancel");
     process.exit(0);
