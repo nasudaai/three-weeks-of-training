a1196fc0ef578c503638942cacfabd928aefcbe5
nasuda <nasuda908@gmail.com>
2026-01-31 08:19:25 +0900

    add argv

--- src/main.mjs
+++ src/main.mjs
@@ -1,7 +1,14 @@
+const args = process.argv.slice(2);
+console.log(args);
 console.log("test");
+
+function write(str) {
+  process.stdout.write(str + "\n"); 
+};
 
 function main() {
   process.stdout.write("main");
+  write("str");
 };
 
 main();
