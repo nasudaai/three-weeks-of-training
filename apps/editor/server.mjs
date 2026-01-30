f82a69db6b2e055066f1503b0b042610bf60df59
nasuda <nasuda908@gmail.com>
2026-01-31 08:12:16 +0900

    add editor dir

---
+++ apps/editor/server.mjs
@@ -0,0 +1,12 @@
+import { createServer } from 'node:http';
+import { readFileSync } from 'node:fs';
+
+const html = readFileSync("index.html");
+
+const server = createServer((req, res) => {
+  res.end(html);
+});
+
+server.listen(3000, () => {
+  console.log("listen on http://localhost:3000");
+});
