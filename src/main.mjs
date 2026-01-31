0683c52b873d0b635fbd9570a858046d8ab052ea
nasuda <nasuda908@gmail.com>
2026-02-01 00:53:39 +0900

    add json

--- src/main.mjs
+++ src/main.mjs
@@ -2,6 +2,15 @@ const args = process.argv.slice(2);
 console.log(args);
 console.log("test");
 
+
+const data = {
+  key: "value"
+};
+
+function reJson(data) {
+  return JSON.stringify(data);
+};
+
 function write(str) {
   process.stdout.write(str + "\n"); 
 };
@@ -9,6 +18,8 @@ function write(str) {
 function main() {
   process.stdout.write("main");
   write("str");
+  const json = reJson(data);
+  write(json);
 };
 
 main();
