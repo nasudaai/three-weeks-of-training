b13bd775dd63beb0c88eb80c43921b8e9cb7e997
nasuda <nasuda908@gmail.com>
2026-02-03 03:56:27 +0900

    add error handling

--- weeks/second-week/scripts/a.mjs
+++ weeks/second-week/scripts/a.mjs
@@ -1,2 +1,9 @@
+if ( process.argv.length >= 3 ) {
+  console.log("test");
+} else {
+  process.stderr.write("please argv")
+  process.exit(1);
+}
+
 const args = process.argv.slice(2);
 console.log(args);
