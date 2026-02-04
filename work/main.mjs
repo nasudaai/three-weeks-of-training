995091ed5ee691141d8ec10cbaef7151fed2674f
nasuda <nasuda908@gmail.com>
2026-02-04 16:15:26 +0900

    budding sign

--- work/main.mjs
+++ work/main.mjs
@@ -1,1 +1,8 @@
+const args = process.argv.slice(2);
+console.log(args);
+
+const logTitls = ["money", "guitar", "calorie", "protein"];
+console.log(logTitls);
+
+
 process.stdout.write("test" + "\n");
