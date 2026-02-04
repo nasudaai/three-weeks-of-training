4d6704b4fc234d276a87f91f439578f31c324e8e
nasuda <nasuda908@gmail.com>
2026-02-04 16:36:08 +0900

    add structure

--- work/main.mjs
+++ work/main.mjs
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 console.log(args);
 
-const logTitls = ["money", "guitar", "calorie", "protein"];
+const logTitls = [ "money", "guitar", { "food-related-keys": ["calorie", "protein"]} ];
 console.log(logTitls);
 
 
