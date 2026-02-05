779678605211bba7d50f80e76f876b9860efa257
nasuda <nasuda908@gmail.com>
2026-02-05 16:02:00 +0900

    comment out

--- work/main.mjs
+++ work/main.mjs
@@ -1,12 +1,14 @@
 const args = process.argv.slice(2);
 console.log(args);
 
+/*
 const logTitls = [ "money", "guitar", { "food-related-keys": ["calorie", "protein"]} ];
 console.log(logTitls);
 
 
 const objVlue = logTitls[2];
 console.log(objVlue);
+*/
 
 
 process.stdout.write("test" + "\n");
