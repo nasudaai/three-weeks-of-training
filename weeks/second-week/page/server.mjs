5e28dc3df45cdd8df9b9998778b2b73d04df56c7
nasuda <nasuda908@gmail.com>
2026-02-05 16:19:35 +0900

    add listen code

--- weeks/second-week/page/server.mjs
+++ weeks/second-week/page/server.mjs
@@ -1,2 +1,12 @@
 import { createServer } from 'node:http';
 console.log(createServer);
+
+const html = "html";
+
+const server = createServer((req, res) => {
+  res.end(html);
+});
+
+server.listen(3000, () => {
+  console.log("listen on http://localhost:3000");
+});
