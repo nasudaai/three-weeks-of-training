88b3058c79750f5c1dc04ac4dfb76d0b5d53852a
nasuda <nasuda908@gmail.com>
2026-02-07 06:26:26 +0900

    add stdin.on

--- weeks/second-week/app.mjs
+++ weeks/second-week/app.mjs
@@ -1,2 +1,8 @@
 const args = process.argv.slice(2);
+if (args[0] === "std") {
+  process.stdin.on("data", (chunk) => {
+
+    process.stdout.write(chunk.toString());
+  });
+}
 process.stdout.write(JSON.stringify(args) + "\n");
