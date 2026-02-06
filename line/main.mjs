389a0f8411294256eaa0716a295c067b90887eca
nasuda <nasuda908@gmail.com>
2026-02-06 16:03:59 +0900

    add imput

--- line/main.mjs
+++ line/main.mjs
@@ -1,6 +1,8 @@
-console.log("test");
+const args = process.argv.slice(2);
+//console.log("test");
 
 const arr = ["a", "b", "c"];
 console.log(arr);
+console.log(args);
 
-process.stdout.write(JSON.stringify(arr + "\n"));
+process.stdout.write(JSON.stringify(arr) + "\n");
