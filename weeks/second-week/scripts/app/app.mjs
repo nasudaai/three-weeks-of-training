c3a43f3c4e0b9883efff802a9af68decde88c574
nasuda <nasuda908@gmail.com>
2026-02-07 15:14:42 +0900

    add early return

--- weeks/second-week/scripts/app/app.mjs
+++ weeks/second-week/scripts/app/app.mjs
@@ -1,15 +1,31 @@
 const args = process.argv.slice(2);
 //console.log(args);
+//console.log(args[0]);
+
+//early return
+if (args[0] !== "memo" && args[0] !== "note") { //if (!["memo", "note"].includes(value)) {
+  
+  process.stdout.write("usage <memo> or <note> <content>...")
+  process.exit(1);
+}
+
+if (args[0] === "memo") {
+  process.stdout.write("append!\n");
+} else if (args[0] === "note") {
+  process.stdout.write("create note!\n");
+}
 
 const obj = {};
 const note = [];
 
+//create note
 for (let i = 0; i < args.length; i++) {
   const memo = args[i];
   note.push(memo);
 }
 console.log(note);
 
+//create object
 const key = args[0];
 const value = args[1];
 obj[key] = value;
