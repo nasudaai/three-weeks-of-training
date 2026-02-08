f393f512b1ca89c12f6e5190b2a0ca7c98283bc3
nasuda <nasuda908@gmail.com>
2026-02-08 10:32:31 +0900

    add create obj

--- weeks/third-week/createObj.mjs
+++ weeks/third-week/createObj.mjs
@@ -1,3 +1,8 @@
+//one key one value
+
+process.stdout.write("default <key> <value>\n"); // mode key | add value | one prop
+
+
 const args = process.argv.slice(2);
 
 const obj = {};
@@ -6,7 +11,7 @@ const obj = {};
 let key = "";
 
 key = args[0];
-obj[key] = 1;
+obj[key] = args[1];
 
 
 console.log(obj);
