2ad9befebccebd9208f951a9028275e2e9c683c7
nasuda <nasuda908@gmail.com>
2026-02-11 23:11:19 +0900

    add code

--- weeks/third-week/app/main.mjs
+++ weeks/third-week/app/main.mjs
@@ -1,5 +1,13 @@
 import { fn } from './func.mjs';
 
+const args = process.argv.slice(2);
+const mode = args[0];
+
+if (mode !== "argv" && mode !== "std") {
+  console.log("usage <argv> or <std>");
+  process.exit(1);
+}
+
 function main() {
   fn();
   process.stdout.write("Test");
